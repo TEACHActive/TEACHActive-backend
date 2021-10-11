14985e5c40a7f240ae4c2afb0f52a5ad49bd73df
Jameel Kelley <jkelley@iastate.edu>
2021-10-11 10:31:49 -0500

    Create and Read generic reflections

--- src/controllers/reflections/index.ts
+++ src/controllers/reflections/index.ts
@@ -1,17 +1,146 @@
 import express from "express";
 import { DateTime } from "luxon";
-import { ReflectionsModel } from "../../models/reflectionsModel";
+import {
+  Better_ReflectionSectionSchema,
+  Better_ReflectionsModel,
+  HandRaisesReflectionModel,
+  ReflectionsModel,
+} from "../../models/reflectionsModel";
+
+import * as Constants from "../../constants";
 
 const app = express();
 
 const baseEndpoint = "/reflections";
 
+const YNQuestion = (yes: string, no: string) => {
+  return {
+    yes: {
+      value: "",
+      label: yes,
+      selected: false,
+    },
+    no: {
+      value: "",
+      label: no,
+      selected: false,
+    },
+  };
+};
+const DefaultYNQuestion = YNQuestion("", "");
+
+const MultiChoiceQuestion = (
+  options: {
+    value: string;
+    label: string;
+    selected: boolean;
+  }[],
+  hasOther: boolean = false,
+  otherValue: string = ""
+) => {
+  return {
+    options: options,
+    hasOther: hasOther,
+    otherValue: otherValue,
+  };
+};
+const DefaultMultiChoiceQuestion = MultiChoiceQuestion(
+  [{ value: "", label: "", selected: false }],
+  false,
+  ""
+);
+
+const SingleChoiceQuestion = (
+  options: {
+    value: string;
+    label: string;
+    selected: boolean;
+  }[],
+  hasOther: boolean = false,
+  otherValue: string = ""
+) => {
+  return {
+    options: options,
+    hasOther: hasOther,
+    otherValue: otherValue,
+  };
+};
+const DefaultSingleChoiceQuestion = SingleChoiceQuestion(
+  [{ value: "", label: "", selected: false }],
+  false,
+  ""
+);
+
+const FreeResponseQuestion = (label: string, value: string) => {
+  return {
+    value: value,
+    label: label,
+    selected: false,
+  };
+};
+const DefaultFreeResponseQuestion = FreeResponseQuestion("", "");
+
+const LikertQuestion = (
+  minValue: number = 1,
+  maxValue: number = 5,
+  value: number = 1,
+  isSet: boolean = false
+) => {
+  return {
+    value: 1,
+    minValue: 1,
+    maxValue: 5,
+    isSet: false,
+  };
+};
+const DefaultLikertQuestion = LikertQuestion();
+
+const baseReflectionSections = [
+  {
+    name: "handRaises",
+    title: "Hand Raises",
+    questions: [
+      {
+        value: "",
+        priority: 0,
+        selected: true,
+        onSelected: {
+          prompt: "This is a prompt",
+          questionType: "singleChoiceQuestion",
+          ynQuestion: DefaultYNQuestion,
+          multiChoiceQuestion: DefaultMultiChoiceQuestion,
+          singleChoiceQuestion: DefaultSingleChoiceQuestion,
+          freeResponseQuestion: DefaultFreeResponseQuestion,
+          likertQuestion: DefaultLikertQuestion,
+        },
+      },
+    ],
+  },
+  {
+    name: "studentSpeech",
+    title: "Student Speech",
+    questions: [],
+  },
+  {
+    name: "instructorSpeech",
+    title: "Instructor Speech",
+    questions: [],
+  },
+];
+
 /**
  * Create/Update a new reflection
  */
 app.put(`${baseEndpoint}/:uid/:sessionId`, async (req, res) => {
+  console.log("Create/Update a new reflection");
   const { uid, sessionId } = req.params;
-  const { reflections } = req.body;
+  let reflections = baseReflectionSections;
+  //Update reflections as necessary
+  if (req.params.reflections && Array.isArray(req.params.reflections)) {
+    req.params.reflections.forEach((incomingReflection) => {
+      console.log(incomingReflection);
+    });
+  }
   if (uid === undefined) {
     console.error("id must be defined");
     res.status(400);
@@ -32,13 +161,13 @@ app.put(`${baseEndpoint}/:uid/:sessionId`, async (req, res) => {
   }
 
   try {
-    const matchingReflection = await ReflectionsModel.findOne({
+    const matchingReflection = await Better_ReflectionsModel.findOne({
       userId: uid,
       sessionId: sessionId,
     });
 
     if (!matchingReflection) {
-      const newReflection = new ReflectionsModel({
+      const newReflection = new Better_ReflectionsModel({
         userId: uid,
         sessionId: sessionId,
         reflections: reflections,
@@ -49,16 +178,17 @@ app.put(`${baseEndpoint}/:uid/:sessionId`, async (req, res) => {
       return;
     }
 
-    const updatedReflection = await ReflectionsModel.updateOne(
+    const updatedReflection = await Better_ReflectionsModel.findOneAndUpdate(
       {
         userId: uid,
         sessionId: sessionId,
       },
-      { reflections: reflections }
+      { reflectionSections: reflections },
+      { upsert: true, new: true }
     );
 
     res.status(200);
-
+    res.json(updatedReflection);
     return;
   } catch (err) {
     console.error(err);
@@ -97,7 +227,7 @@ app.get(`${baseEndpoint}/:uid/:sessionId`, async (req, res) => {
   }
 
   try {
-    const matchingReflection = await ReflectionsModel.findOne({
+    const matchingReflection = await Better_ReflectionsModel.findOne({
       userId: uid,
       sessionId: sessionId,
     });
@@ -111,6 +241,198 @@ app.get(`${baseEndpoint}/:uid/:sessionId`, async (req, res) => {
         detail: JSON.stringify(errorMsg),
       });
       return;
+    }
+    res.status(200);
+    res.json(matchingReflection);
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+    res.json({ error: JSON.stringify(err) });
+    return;
+  }
+});
+
+//=================================================================================================
+//=================================================================================================
+
+/**
+ * Create HandRaises template reflection
+ */
+app.post(`${baseEndpoint}/handRaises/:uid/:sessionId`, async (req, res) => {
+  const { uid, sessionId } = req.params;
+  const {
+    handRaisesReasonOptions,
+    handRaiseReasonOther,
+    satisifedWithHandRaisesValue,
+    reasonDissatisfiedWithHandRaises,
+    goalNextSessionValue,
+    handRaiseGoalOptions,
+    handRaiseGoalOther,
+  } = req.body;
+
+  if (uid === undefined) {
+    console.error("id must be defined");
+    res.status(400);
+    res.json({
+      error: "Must Provide userId",
+      detail: "Must Provide userId",
+    });
+    return;
+  }
+  if (sessionId === undefined) {
+    console.error("sessionId must be defined");
+    res.status(400);
+    res.json({
+      error: "Must Provide sessionId",
+      detail: "Must Provide sessionId",
+    });
+    return;
+  }
+
+  try {
+    const result = await HandRaisesReflectionModel.findOneAndUpdate(
+      {
+        userId: uid,
+        sessionId: sessionId,
+      },
+      {
+        userId: uid,
+        sessionId: sessionId,
+        handRaisesReasonOptions: handRaisesReasonOptions,
+        handRaiseReasonOther: handRaiseReasonOther,
+        satisifedWithHandRaisesValue: satisifedWithHandRaisesValue,
+        reasonDissatisfiedWithHandRaises: reasonDissatisfiedWithHandRaises,
+        goalNextSessionValue: goalNextSessionValue,
+        handRaiseGoalOptions: handRaiseGoalOptions,
+        handRaiseGoalOther: handRaiseGoalOther,
+      },
+      { upsert: true }
+    );
+
+    res.status(201);
+    res.json(result);
+    return;
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+    res.json({
+      error: "error when creating/updating reflection",
+      detail: JSON.stringify(err),
+    });
+    return;
+  }
+});
+
+const DefaultHandRaiseGoalOptions = {
+  handRaisesReasonOptions: [
+    {
+      label: "Ask questions/clarifications",
+      value: "questions_clarifications",
+      disabled: false,
+      checked: false,
+    },
+    {
+      label: "Participate in-class discussions",
+      value: "participate_discussions",
+      disabled: false,
+      checked: false,
+    },
+    {
+      label: "Other",
+      value: "other",
+      disabled: false,
+      checked: false,
+    },
+  ],
+  handRaiseReasonOther: "",
+  satisifedWithHandRaisesValue: "",
+  reasonDissatisfiedWithHandRaises: "",
+  goalNextSessionValue: "",
+  handRaiseGoalOptions: [
+    {
+      label: "Trigger more student discussions to increase hand raises",
+      value: "more_student_discussions",
+      disabled: false,
+      checked: false,
+    },
+    {
+      label: "Other",
+      value: "other",
+      disabled: false,
+      checked: false,
+    },
+  ],
+  handRaiseGoalOther: "",
+};
+
+/**
+ * Get HandRaises template reflection
+ */
+app.get(`${baseEndpoint}/handRaises/:uid/:sessionId`, async (req, res) => {
+  const { uid, sessionId } = req.params;
+
+  if (uid === undefined) {
+    console.error("id must be defined");
+    res.status(400);
+    res.json({
+      error: "Must Provide userId",
+      detail: "Must Provide userId",
+    });
+    return;
+  }
+  if (sessionId === undefined) {
+    console.error("sessionId must be defined");
+    res.status(400);
+    res.json({
+      error: "Must Provide sessionId",
+      detail: "Must Provide sessionId",
+    });
+    return;
+  }
+
+  try {
+    let matchingReflection;
+    if (Constants.ADMIN_LIST.includes(uid)) {
+      //User making request is an admin, only match on sessionID
+      matchingReflection = await HandRaisesReflectionModel.findOne({
+        sessionId: sessionId,
+      });
+    } else {
+      //User making request is not an admin
+      matchingReflection = await HandRaisesReflectionModel.findOne({
+        userId: uid,
+        sessionId: sessionId,
+      });
+    }
+    // console.log(matchingReflection);
+
+    if (!matchingReflection) {
+      const newReflection = new HandRaisesReflectionModel({
+        userId: uid,
+        sessionId: sessionId,
+        handRaisesReasonOptions:
+          DefaultHandRaiseGoalOptions.handRaisesReasonOptions,
+        handRaiseReasonOther: DefaultHandRaiseGoalOptions.handRaiseReasonOther,
+        satisifedWithHandRaisesValue:
+          DefaultHandRaiseGoalOptions.satisifedWithHandRaisesValue,
+        reasonDissatisfiedWithHandRaises:
+          DefaultHandRaiseGoalOptions.reasonDissatisfiedWithHandRaises,
+        goalNextSessionValue: DefaultHandRaiseGoalOptions.goalNextSessionValue,
+        handRaiseGoalOptions: DefaultHandRaiseGoalOptions.handRaiseGoalOptions,
+        handRaiseGoalOther: DefaultHandRaiseGoalOptions.handRaiseGoalOther,
+      });
+      const savedReflection = await newReflection.save();
+      res.json(savedReflection);
+      return;
+
+      // const errorMsg = `User with id: ${uid} and sessionId ${sessionId} not found`;
+      // console.error(errorMsg);
+      // res.status(404);
+      // res.json({
+      //   error: "could not find matching hand raises reflection",
+      //   detail: JSON.stringify(errorMsg),
+      // });
+      // return;
     }
     res.status(200);
     res.json(matchingReflection);
--- src/models/reflectionsModel.ts
+++ src/models/reflectionsModel.ts
@@ -4,15 +4,18 @@ const Schema = mongoose.Schema;
 
 const NestedChoiceQuestionOptionSchema = {
   value: String,
+  label: String,
   selected: Boolean,
 };
 
 const ChoiceQuestionOptionSchema = new Schema({
   value: {
+    //The response to the question
     type: String,
     required: true,
   },
-  selected: Boolean,
+  priority: Number, //The order of each question in the list (eg. can reorder questions this way). Higher is closer to top
+  selected: Boolean, //If the question should be displayed
   onSelected: new Schema({
     prompt: {
       type: String,
@@ -41,6 +44,12 @@ const ChoiceQuestionOptionSchema = new Schema({
     },
     freeResponseQuestion: {
       feild: NestedChoiceQuestionOptionSchema,
+      required: false,
+    },
+    likertQuestion: {
+      value: NestedChoiceQuestionOptionSchema,
+      minValue: NestedChoiceQuestionOptionSchema,
+      maxValue: NestedChoiceQuestionOptionSchema,
       required: false,
     },
   }),
@@ -106,4 +115,60 @@ export const ReflectionsSchema = new Schema({
 
 const ReflectionsModel = mongoose.model("ReflectionsModel", ReflectionsSchema);
 
-export { ReflectionsModel };
+const HandRaisesOptionsSchema = new Schema({
+  label: String,
+  value: String,
+  disabled: Boolean,
+  checked: Boolean,
+});
+
+export const HandRaisesReflectionSchema = new Schema({
+  userId: {
+    type: String,
+  },
+  sessionId: {
+    type: String,
+  },
+  handRaisesReasonOptions: [HandRaisesOptionsSchema],
+  handRaiseReasonOther: String,
+  satisifedWithHandRaisesValue: String,
+  reasonDissatisfiedWithHandRaises: String,
+  goalNextSessionValue: String,
+  handRaiseGoalOptions: [HandRaisesOptionsSchema],
+  handRaiseGoalOther: String,
+});
+
+const HandRaisesReflectionModel = mongoose.model(
+  "HandRaisesReflectionModel",
+  HandRaisesReflectionSchema
+);
+
+//=========================================================================================
+//=========================================================================================
+
+const Better_ReflectionSectionSchema = new Schema({
+  name: String,
+  title: String,
+  questions: [ChoiceQuestionOptionSchema],
+});
+
+const Better_ReflectionsSchema = new Schema({
+  userId: String,
+  sessionId: String,
+  reflectionSections: [Better_ReflectionSectionSchema],
+});
+
+const Better_ReflectionsModel = mongoose.model(
+  "Better_ReflectionsModel",
+  Better_ReflectionsSchema
+);
+
+//=========================================================================================
+//=========================================================================================
+
+export {
+  ReflectionsModel,
+  HandRaisesReflectionModel,
+  Better_ReflectionsModel,
+  Better_ReflectionSectionSchema,
+};
